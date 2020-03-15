2b496cb2e9e6bcc9ec3b226ad0a0871aed241887
Saban Kucuk <sabanku34@gmail.com>
2020-03-14 18:34:49 -0700

    commit 10

--- src/gitPackage/NextClass.java
+++ src/gitPackage/NextClass.java
@@ -3,7 +3,10 @@ package gitPackage;
 public class NextClass {
 
 	public static void main(String[] args) {
-	System.out.println("Hello dear friends");
+	System.out.println("Hello dear Testers");
+	
+	
+	
 	}
 
 }
